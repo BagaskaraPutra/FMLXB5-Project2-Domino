d806f9ef3bd9009fdb25302d2be72bc6b4bacd6d
BagaskaraPutra <primastyaputra@gmail.com>
2023-10-19 12:02:25 +0700

    Update unit test

--- DominoGame/DominoConsole.Test/UnitTest1.cs
+++ DominoGame/DominoConsole.Test/UnitTest1.cs
@@ -1,39 +1,58 @@
 ﻿using DominoConsole;
+using Moq;
 namespace DominoConsole.Test;
 
-public class UnitTest1
+public class GameControllerTest
 {
-	GameController? game;
-	IPlayer? currentPlayer;
-	List<Card>? cardsList;
-	Card? playerCard;
-	HashSet<IdNodeSuit>? openNodes;
-	IdNodeSuit targetIdNodeSuit;
-	List<KeyValuePair<Card, IdNodeSuit>>? deckTableCompatible;
-	DominoTree? dominoTree;
-	CardGUI? cardGUI;
-	TableGUI? tableGUI;
-	List<CardKinematics>? cardKinematicsLUT;
-	[Fact]
-	public void AddPlayerSuccessTest()
+	private GameController _cut;
+	private Mock<IPlayer> _player;
+	private Mock<ICard> _card;
+	
+	public GameControllerTest()
 	{
-		game = new(3, 100);
-		game.AddPlayer(new Player(id: 1, name: "Alpha"));
-		game.AddPlayer(new Player(id: 2, name: "Beta"));
-		game.AddPlayer(new Player(id: 3, name: "Charlie"));
-		Assert.Equal(3, game.GetPlayers().Count);
+		_cut = new(3, 100);	
+	}
+	
+	[Fact]
+	public void AddPlayer_PlayerNumberEqualsExpected_ThreePlayers()
+	{
+		_cut.AddPlayer(new Player(id: 1, name: "Alpha"));
+		_cut.AddPlayer(new Player(id: 2, name: "Beta"));
+		_cut.AddPlayer(new Player(id: 3, name: "Charlie"));
+		Assert.Equal(3, _cut.GetPlayers().Count);
 	}
 	[Fact]
-	public void AddPlayerFailTest()
+	public void AddPlayer_PlayerNumberEqualsExpected_FourPlayers()
 	{
-		game = new(3, 100);
-		game.AddPlayer(new Player(id: 1, name: "Alpha"));
-		game.AddPlayer(new Player(id: 1, name: "Alpha"));
-		game.AddPlayer(new Player(id: 3, name: "Charlie"));
-		foreach(var player in game.GetPlayers())
+		_cut = new(4, 100);
+		_cut.AddPlayer(new Player(id: 1, name: "Alpha"));
+		_cut.AddPlayer(new Player(id: 2, name: "Bravo"));
+		_cut.AddPlayer(new Player(id: 3, name: "Charlie"));
+		_cut.AddPlayer(new Player(id: 4, name: "Delta"));
+		Assert.Equal(4, _cut.GetPlayers().Count);
+	}
+	[Fact]
+	public void AddPlayer_PlayerNumberNotEqualsExpected_OnePlayerTryDuplicate()
+	{
+		Player alphaPlayer = new Player(id: 1, name: "Alpha");
+		_cut.AddPlayer(alphaPlayer);
+		_cut.AddPlayer(alphaPlayer);
+		_cut.AddPlayer(new Player(id: 1, name: "Alpha"));
+		Console.WriteLine($"Max number of players: {_cut.MaxNumPlayers}");
+		foreach (var player in _cut.GetPlayers())
 		{
 			Console.WriteLine($"{player.GetId()}: {player.GetName()}");
 		}
-		Assert.NotEqual(3, game.GetPlayers().Count);
+		Assert.NotEqual(3, _cut.GetPlayers().Count);
 	}
+	// [Fact]
+	// public void AddCards_CardsAddedSuccessfullyReturnsTrue_PlayerExists()
+	// {
+	// 	_card.Setup(c => c.SetStatus(It.IsAny<CardStatus>()));
+
+	// 	bool result = _cut.AddCards(_player.Object, _card.Object);
+
+	// 	Assert.IsTrue(result);
+	// 	_card.Verify(c => c.SetStatus(It.IsAny<CardStatus>()), Times.Once);
+	// }
 }
